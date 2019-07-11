022965b67d0ff7e4c7957b3620e32c3e5a3b2cfc
DgurJIupyf <Dgurjiupyff@mail.ru>
2019-07-12 01:58:56 +0300

    Add a=0 and b=0

--- app/src/StartBigFile.js
+++ app/src/StartBigFile.js
@@ -2,14 +2,24 @@ import React from "react";
 
 export function Page () {
     return (<div>
-        {calcQuadratic(1, -2, -4)}
+        {calcQuadratic(1, 0, -4)}
     </div>)
 }
 
 function calcQuadratic ( a, b, c ) {
+    if (a === 0) {
+        const x = Math.round((-c/b)*100)/100
+        return x
+    }
+
+    else if (b === 0) {
+        const x = Math.round((Math.sqrt(Math.abs(c/a)))*100)/100
+        const roots = [-x, x]
+        return roots
+    }
+
     const Dis = b*b -4*a*c 
     console.log(Dis)
-
     if (Number.isNaN(Math.sqrt(Dis)))
         return "Введены неверные значения при которых дискриминант отрицательный"
     else {
