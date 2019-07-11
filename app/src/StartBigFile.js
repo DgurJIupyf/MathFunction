fda84d0f12af37d2f92dfc9cb0af29d9aa22d029
DgurJIupyf <Dgurjiupyff@mail.ru>
2019-07-11 23:01:58 +0300

    fast save

--- app/src/StartBigFile.js
+++ app/src/StartBigFile.js
@@ -2,9 +2,12 @@ import React from "react";
 
 export function Page () {
     return (<div>
-        {calcQuadratic(1, 4, 3)}
+        {calcQuadratic(2, -2, -3)}
     </div>)
 }
+
+console.log(Math.round(1.085555555*100)/100)
+
 
 function makeRoundNumber ( Dis ) {
     let [intNum, floatNum] = Dis.toString().split(".")
@@ -20,16 +23,27 @@ function makeRoundNumber ( Dis ) {
 
 function calcQuadratic ( a, b, c ) {
     const Dis = b*b -4*a*c 
-    let sqrtDis = Math.sqrt(Dis)
+    console.log(Dis)
 
-    if (Number.isInteger(Math.sqrt(Dis))) 
-        sqrtDis =  sqrtDis/(2*a)
-    else 
-        sqrtDis = makeRoundNumber(sqrtDis/(2*a))
+    if (Math.sqrt(Dis) === NaN)
+        return "Введены неверные значения при которых дискриминант отрицательный"
+    else {
+        let sqrtDis = Math.sqrt(Dis)
+        console.log(sqrtDis, Math.sqrt(Dis), Dis)
 
-    let x1 = -b - sqrtDis
-    let x2 = -b + sqrtDis
-    let roots = [x1, x2]
+        if (Number.isInteger(Math.sqrt(Dis))) 
+            sqrtDis =  sqrtDis/(2*a)
+        else{ 
+            console.log (sqrtDis)
+            sqrtDis = makeRoundNumber(sqrtDis/(2*a))
+        }
 
-    return roots
+        let x1 = -b - sqrtDis
+        let x2 = -b + sqrtDis
+        let roots = [x1, x2]
+
+        console.log (sqrtDis, sqrtDis/(2*a))
+
+        return roots
+    }
 }
